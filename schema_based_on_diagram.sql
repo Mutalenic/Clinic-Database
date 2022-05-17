c16f547de44448b7e1222213c6194ac02e1b0714
Nicholas Mutale <nicomutale@gmail.com>
2022-05-17 16:59:12 +0200

    Add medical history table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -3,3 +3,11 @@ id INT GENERATED ALWAYS AS IDENTITY,
 name VARCHAR(200),
 date_of_birth date,
 PRIMARY KEY(id));
+
+CREATE TABLE medical_histories(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    admitted_at timestamp,
+    patient_id INT REFERENCES patients(id),
+    status VARCHAR(300),
+    PRIMARY KEY(id)
+);
