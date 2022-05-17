f6905fc169b9a6f52e7ab3a75d6b15a4dc74d791
Ismail courr <25227022+Ismailco@users.noreply.github.com>
2022-05-17 17:34:27 +0100

    Create a join table for many-to-many relastionship tables and Add indexes

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -38,6 +38,13 @@ CREATE TABLE invoice_items(
     PRIMARY KEY(id)
 );
 
+CREATE TABLE medical_histories_and_treatments (
+    medical_history_id INT REFERENCES medical_histories(id),
+    treatment_id INT REFERENCES treatments(id),
+);
+
+CREATE INDEX medical_history_id_index ON medical_histories_and_treatments(medical_history_id ASC) ;
+CREATE INDEX treament_id_index ON medical_histories_and_treatments(treatment_id ASC) ;
 CREATE INDEX patient_id_index ON medical_histories(patient_id ASC) ;
 CREATE INDEX medical_history_id_index ON invoices(medical_history_id ASC) ;
 CREATE INDEX invoice_id_index ON invoice_items(invoice_id ASC);
