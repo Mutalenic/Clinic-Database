d88538491c972f5d03ef97cfc60c626c597b98fb
Nicholas Mutale <nicomutale@gmail.com>
2022-05-17 17:10:40 +0200

    Add treatments table Add invoices item table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -11,3 +11,29 @@ CREATE TABLE medical_histories(
     status VARCHAR(300),
     PRIMARY KEY(id)
 );
+
+CREATE TABLE invoices(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    total_amount decimal,
+    generated_at timestamp,
+    payed_at timestamp,
+    medical_history_id INT REFERENCES medical_histories(id),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE treatments(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    type VARCHAR,
+    name VARCHAR,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE invoice_items(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    unit_price decimal,
+    quantity INT,
+    total_price decimal,
+    invoice_id INT REFERENCES invoices(id),
+    treament_id INT REFERENCES treatments(id),
+    PRIMARY KEY(id)
+);
